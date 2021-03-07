5bce0e0b4431a67167200f635e302e5c14aac582
vgruzas <vgruzas07@gmail.com>
2021-03-07 12:09:44 +0200

    Maven pakeitimas

--- src/test/java/basics/AssertionExamples.java
+++ src/test/java/basics/AssertionExamples.java
@@ -34,7 +34,7 @@ public class AssertionExamples {
 
         int result = calculator.multiplication(10,50);
 
-        Assertions.assertEquals(500,result);
+        Assertions.assertEquals(100,result);
     }
 
     @Test
